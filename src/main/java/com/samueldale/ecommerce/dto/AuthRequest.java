bea790df9bf5fda20a90d82b1dd0eff89a877aa1
Samuel Dale <samueldale.dampil@gmail.com>
2023-07-31 00:49:23 +0800

    Added JWT Authentication and Authorization

---
+++ src/main/java/com/samueldale/ecommerce/dto/AuthRequest.java
@@ -0,0 +1,18 @@
+package com.samueldale.ecommerce.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
